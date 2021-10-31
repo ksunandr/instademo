1da3efda1294050355b487ad4b78a10e11ce07fe
Kseniia Andreeva <kseniia.andreeva@netcracker.com>
2021-10-31 22:46:10 +0300

    entities

--- src/main/java/org/ksun/pets/udemy/course/instademo/entity/Comment.java
+++ src/main/java/org/ksun/pets/udemy/course/instademo/entity/Comment.java
@@ -1,15 +1,26 @@
 package org.ksun.pets.udemy.course.instademo.entity;
 
-import javax.persistence.PrePersist;
+import lombok.Data;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Data
+@Entity
 public class Comment {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @ManyToOne(fetch = FetchType.EAGER)
     private Post post;
+    @Column(nullable = false)
     private String userName;
+    @Column(nullable = false)
     private Long userId;
+    @Column(columnDefinition = "text", nullable = false)
     private String message;
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @PrePersist
--- src/main/java/org/ksun/pets/udemy/course/instademo/entity/Post.java
+++ src/main/java/org/ksun/pets/udemy/course/instademo/entity/Post.java
@@ -1,23 +1,41 @@
 package org.ksun.pets.udemy.course.instademo.entity;
 
-import javax.persistence.PrePersist;
+import lombok.Data;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
+@Data
+@Entity
+
 public class Post {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
     private String caption;
     private String loation;
     private Integer likes;
 
-    private Set<User> likedUsers = new HashSet<>();
+    @Column
+    @ElementCollection(targetClass = String.class)
+    private Set<String> likedUsers = new HashSet<>();
+
+    @ManyToOne(fetch = FetchType.LAZY)
     private User user;
+
+    @OneToMany(cascade = CascadeType.REFRESH,
+            fetch = FetchType.EAGER,
+            mappedBy = "post",
+            orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
+
+    @Column(updatable = false)
     private LocalDateTime createdDate;
 
     @PrePersist
