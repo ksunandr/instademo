4c4660cb29e70dcf98549e00067a18e75405bc46
Kseniia Andreeva <kseniia.andreeva@netcracker.com>
2021-11-06 13:09:20 +0300

    security 2

---
+++ src/main/java/org/ksun/pets/udemy/course/instademo/payload/response/InvalidLoginResponse.java
@@ -0,0 +1,13 @@
+package org.ksun.pets.udemy.course.instademo.payload.response;
+
+public class InvalidLoginResponse {
+
+    private String username;
+    private String password;
+
+    public InvalidLoginResponse(){
+        this.username = "Invalid Username";
+        this.password = "Invalid Password";
+
+    }
+}
