6f1fb2d58775c0c5ff5b842f1a76a57556f6c619
Kseniia Andreeva <kseniia.andreeva@netcracker.com>
2021-11-12 18:54:48 +0300

    UserService

---
+++ src/main/java/org/ksun/pets/udemy/course/instademo/service/UserService.java
@@ -0,0 +1,44 @@
+package org.ksun.pets.udemy.course.instademo.service;
+
+import org.ksun.pets.udemy.course.instademo.entity.User;
+import org.ksun.pets.udemy.course.instademo.entity.enums.ERole;
+import org.ksun.pets.udemy.course.instademo.payload.request.SignupRequest;
+import org.ksun.pets.udemy.course.instademo.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    public static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
+
+    private final UserRepository userRepository;
+    private final BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    public UserService(UserRepository userRepository, BCryptPasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    public User createUser(SignupRequest userIn) {
+        User user = new User();
+        user.setEmail(userIn.getEmail());
+        user.setName(userIn.getFirstname());
+        user.setLastname(userIn.getLastname());
+        user.setUsername(userIn.getUsername());
+        user.setPassword(passwordEncoder.encode(userIn.getPassword()));
+        user.getRoles().add(ERole.ROLE_USER);
+
+        try {
+            LOGGER.info("Save user {}", userIn.getEmail());
+            return userRepository.save(user);
+        } catch (Exception e) {
+            LOGGER.error("Error during registration {}", e.getMessage());
+            throw new RuntimeException("The user "+user.getUsername()+" already exist.");
+        }
+    }
+}
